704a731887b7340595cd55aec2fd35328b295a94
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-06-19 20:21:19 -0400

    finish step 2

--- step1.js
+++ step1.js
@@ -1,16 +1,15 @@
 const fs = require("fs");
+const process = require("process");
 
 function cat(path) {
-  try {
-    // store the read file contents
-    let contents = fs.readFileSync(path, "utf8");
-    console.log(`file contents are "${contents}"`);
-  } catch (error) {
-    // errors thrown by fs will be caught here
-    console.error(error);
-    // kill the process and tell the shell it errored
-    process.exit(1);
-  }
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
-cat("one.txt");
+cat(process.argv[2]);
